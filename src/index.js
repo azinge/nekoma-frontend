ca4e1e51942e1f9dccebf8d95cc061d389505f10
cazinge <eddie.azinge@gmail.com>
2020-01-19 13:58:05 -0800

    Demoing for teddy

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheck, faTimes, faCoffee, faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faCheck,
+  faTimes,
+  faCoffee,
+  faArrowLeft,
+} from '@fortawesome/free-solid-svg-icons';
 import './index.css';
 import App from './App';
+import ApolloClient from 'apollo-boost';
+import { gql } from 'apollo-boost';
 
-library.add(faCheck, faTimes, faCoffee, faArrowLeft)
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/',
+});
+
+library.add(faCheck, faTimes, faCoffee, faArrowLeft);
+
+client
+  .query({
+    query: gql`
+      {
+        books {
+          title
+          author
+        }
+      }
+    `,
+  })
+  .then(result => console.log(result));
 
 ReactDOM.render(
   <BrowserRouter>
--- src/views/sales/Sales.js
+++ src/views/sales/Sales.js
@@ -1,50 +1,54 @@
-import React, { useState, useEffect } from 'react'
-import SalesTable from '../../tables/SalesTable'
-import Header from '../../components/Header'
+import React, { useState, useEffect } from 'react';
+// import SalesTable from '../../tables/SalesTable'
+import Header from '../../components/Header';
 
 const Sales = props => {
-  const eventId = props.match.params.id
+  const eventId = props.match.params.id;
 
-  console.log('fetching ' + eventId)
+  console.log('fetching ' + eventId);
   const initialEventSalesState = {
     eventSales: {},
     loading: true,
-  }
+  };
 
   const salesData = [
-    { id: 1, day: 2, items: [
-      'sku-1',
-      'sku-3',
-    ], total: 20, cc: true, female: false },
-  ]
+    {
+      id: 1,
+      day: 2,
+      items: ['sku-1', 'sku-3'],
+      total: 20,
+      cc: true,
+      female: false,
+    },
+  ];
 
-  const [sales, setSales] = useState(initialEventSalesState)
+  const [sales, setSales] = useState(initialEventSalesState);
 
   useEffect(() => {
-    const getSales = async() => {
+    const getSales = async () => {
       // should have await function getting sales for id
-      const data = salesData
+      const data = salesData;
 
-      setSales(data)
-    }
-    getSales()
-  }, [])
+      setSales(data);
+    };
+    getSales();
+  }, []);
 
   const deleteSale = id => {
-    setSales(sales.filter(sale => sale.id !== id))
-  }
+    setSales(sales.filter(sale => sale.id !== id));
+  };
 
   return (
-    <div className="full-container">
-      <Header title={"Sales for event " + eventId} back="/Events" />
-      <div className="container">
+    <div className='full-container'>
+      <Header title={'Sales for event ' + eventId} back='/Events' />
+      <div className='container'>
         <button>Add new sale</button>
-        <div className="flex-large">
+        <div className='flex-large'>
           {/* <SalesTable sales={sales} deleteSale={deleteSale} /> */}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sales
+export default Sales;
